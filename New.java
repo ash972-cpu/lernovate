781d8617f58bdc860135f873a0634736157e4bca
ash972-cpu <ash27mishra@gmail.com>
2025-05-19 09:59:18 +0530

    Add files via upload

---
+++ New.java
@@ -0,0 +1,21 @@
+public class New
+{
+    public static void main (String args []){
+        int a = 24;
+        int b = 78;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a%b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a<50&&b>60);
+        System.out.println(a<b||a==b);
+        System.out.println(++a);
+        System.out.println(--a);
+        System.out.println(a&b);
+        int result = ( a>b)?a:b;
+        System.out.println(result);
+    }
+}
