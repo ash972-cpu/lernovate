652a3b9336f9938e798843ba64e6dbe51396066c
ash972-cpu <ash27mishra@gmail.com>
2025-05-19 10:01:30 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,13 @@
+public class Add{
+    public static void max(int num1,int  num2){
+        if(num1>num2){
+            System.out.print(num1);
+               }
+               else{
+                System.out.print(num2);
+               }
+    }
+    public static void main(String args[]){
+        max(1,2);
+    }
+}
